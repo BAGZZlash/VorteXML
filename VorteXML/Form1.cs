ed3e7654ff34b6cc6c15d5a99d843e7bf5cb81cf
BAGZZlash@gmx.de <BAGZZlash@gmx.de>
2021-11-17 17:46:50 +0100

    Markus' code.

--- VorteXML/Form1.cs
+++ VorteXML/Form1.cs
@@ -19,31 +19,34 @@ namespace VorteXML
 
         private void button1_Click(object sender, EventArgs e)
         {
-            System.Xml.Linq.XDocument MyXML = System.Xml.Linq.XDocument.Load("C:\\Temp\\Cloud\\ownCloud\\WFLO\\Vortex\\Node-Beschreibungs-Theorie\\VorteXML.xml");
+            //System.Xml.Linq.XDocument MyXML = System.Xml.Linq.XDocument.Load("C:\\Temp\\Cloud\\ownCloud\\WFLO\\Vortex\\Node-Beschreibungs-Theorie\\VorteXML.xml");
+            System.Xml.Linq.XDocument MyXML = System.Xml.Linq.XDocument.Load("C:\\Users\\ccroo\\ownCloud\\WFLO\\Vortex\\Node-Beschreibungs-Theorie\\VorteXML.xml");
 
-            var MyQuery = from c in MyXML.Root.Descendants("Element")
-                        //where (int)c.Attribute("rowNr") < 4
-                        select c.Element("InputTypes").Value;
+            System.Xml.Linq.XNamespace elementNS = "Element";
 
-            foreach (string MyData in MyQuery)
+            var MyQuery = from c in MyXML.Root.Descendants(elementNS + "Input")
+
+            select c.Element("InputTypes").Elements();
+
+            foreach (System.Xml.Linq.XElement MyData in MyQuery.First())
             {
-                MessageBox.Show("Data: {0}", MyData);
+                MessageBox.Show(MyData.Name.ToString());
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             System.Xml.XmlDocument MyDoc = new System.Xml.XmlDocument();
-            MyDoc.Load("C:\\Temp\\Cloud\\ownCloud\\WFLO\\Vortex\\Node-Beschreibungs-Theorie\\VorteXML.xml");
+            //MyDoc.Load("C:\\Temp\\Cloud\\ownCloud\\WFLO\\Vortex\\Node-Beschreibungs-Theorie\\VorteXML.xml");
+            MyDoc.Load("C:\\Users\\ccroo\\ownCloud\\WFLO\\Vortex\\Node-Beschreibungs-Theorie\\VorteXML.xml");
 
-            System.Xml.XmlNode MyNode = MyDoc.DocumentElement.SelectSingleNode("NodeElements/Element");
+            System.Xml.XmlNode MyNode = MyDoc.DocumentElement.SelectSingleNode("Element");
 
             foreach (System.Xml.XmlNode AnotherNode in MyDoc.DocumentElement.ChildNodes)
             {
                 string MyText = AnotherNode.InnerText;
                 MessageBox.Show(MyText);
             }
-
         }
     }
 }
