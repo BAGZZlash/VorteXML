7f6ffb5b50f8dd7b4028085677e0550bbee30d1b
BAGZZlash@gmx.de <BAGZZlash@gmx.de>
2021-11-24 12:53:49 +0100

    Introducing structs.

---
+++ VorteXML/VorteXML.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VorteXML
+{
+    class VorteXML
+    {
+        public enum RowType
+        {
+            Input,
+            Output,
+            Control
+        }
+
+        public enum InputType
+        {
+            // Vector types
+            VectorPoint,
+            VectorLine,
+            VectorPolygon
+
+            // e.g. raster types
+            // ...
+        }
+
+        public struct InputRow
+        {
+            InputType[] inputTypes;
+
+        }
+
+        public struct ToolRow
+        {
+            int Index;
+            RowType rowType;
+
+        }
+
+        private ToolRow[] ToolRows;
+    }
+}
